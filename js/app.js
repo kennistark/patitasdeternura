d6ceb3c5a314ac62040b954b1d4481d4654e3073
DanieldiChiloe <daniel.pena@dichiloe.cl>
2021-02-10 15:17:00 -0300

    menu 0.1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu-toggle")
+const menuBtn = document.querySelector(".navbar-toggle")
+const menu = document.querySelector(".menu-toggle")
+
+menuBtn.onclick = () => {
+    menu.classList.add(".menu-toggle")
+}
